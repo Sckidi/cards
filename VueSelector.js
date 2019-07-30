8af6458b9f37c607f38b23f1f27de91a19bac6e5
Konstantin Lebejko <konstantin.lebejko@gmail.com>
2019-07-30 13:20:31 +0300

    moved from vue-in-html to SFC

---
+++ VueSelector.js
@@ -0,0 +1,25 @@
+let VueSelector = Vue.component('vue-selector', {
+  template: `
+    <div class="custom-vue-selector programming-languages">
+      <p class="custom-vue-selector__label" @click="handleSelectorLabelClick('programmingLanguages')"> {{ title }} </p>
+      <ul class="custom-vue-selector__selector" v-bind:class="{ active: isOpened }">
+        <li class="custom-vue-selector__selector__item" v-for="language in values">{{language}}</li>
+      </ul>
+    </div>
+  `,
+  data() {
+    return {
+      isOpened: false
+    }
+  },
+  props: {
+    values: Array,
+    title: String
+  },
+  methods: {
+    handleSelectorLabelClick() {
+      // Reverses current value of state
+      this.isOpened = !this.isOpened;
+    }
+  },
+})
