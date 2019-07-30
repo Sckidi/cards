120e26aa97f66b586a9871473e2a6aa78c8648bb
Kirill <sckidi99@gmail.com>
2019-07-30 12:20:02 +0300

    Create VueSelector.vue

---
+++ VueSelector.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p> name of coloumn </p>
+      <ul>
+        <li>list</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return{
+      
+    }
+  }
+}
+</script>
+
