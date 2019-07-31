f79d928a08b0fec7261db555369187196ea5e6cb
Konstantin Lebejko <konstantin.lebejko@gmail.com>
2019-07-31 17:58:36 +0300

    child -> parent data passing

--- VueSelector.js
+++ VueSelector.js
@@ -1,15 +1,17 @@
 let VueSelector = Vue.component('vue-selector', {
   template: `
-    <div class="custom-vue-selector programming-languages">
-      <p class="custom-vue-selector__label" @click="handleSelectorLabelClick('programmingLanguages')"> {{ title }} </p>
+    <div class="custom-vue-selector">
+      <p class="custom-vue-selector__label"> {{ title }} </p>
+      <p class="custom-vue-selector__value" @click="handleSelectorLabelClick('programmingLanguages')">{{selectedValue}}</p>
       <ul class="custom-vue-selector__selector" v-bind:class="{ active: isOpened }">
-          <li class="custom-vue-selector__selector__item" v-bind:class="{selectedProgrammingLanguage}" @click="handleSelectorLabelClick('programmingLanguages')" v-for="language in values">{{language}}</li>
+          <li class="custom-vue-selector__selector__item" v-on:click="handleSelectorElementClick(language)" v-for="language in values">{{language}}</li>
       </ul>
     </div>
   `,
   data() {
     return {
-      isOpened: false
+      isOpened: false,
+      selectedValue: 'Nothing selected'
     }
   },
   props: {
@@ -21,8 +23,13 @@ let VueSelector = Vue.component('vue-selector', {
       // Reverses current value of state
       this.isOpened = !this.isOpened;
     },
-    selectedValue () {
-        this.$emit(selectedProgrammingLanguage)
+    handleSelectorElementClick(value) {
+      // Assigns selected value to var in state
+      this.selectedValue = value;
+      // Send data to parent
+      this.$emit('value-selected', this.title, value);
+      // Close selector
+      this.isOpened = !this.isOpened;
     }
   },
 })
