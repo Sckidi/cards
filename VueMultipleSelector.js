7a90fea00085d896998d7b71b8cf2e022e444a8e
Kirill <sckidi99@gmail.com>
2019-08-08 16:20:44 +0300

    columns + adaptive

--- VueMultipleSelector.js
+++ VueMultipleSelector.js
@@ -1,28 +1,26 @@
 let VueSelector = Vue.component('vue-multiple-selector', {
     template: `
     <div class="multiple-selector">
-            <p class="multiple-selector__titles">Programming languages</p>
+            <p class="multiple-selector__titles" @click="handleSelectorLabelClick('programmingLanguages')">{{selected_value}} </p>
             <hr width="85%" size="1" color=#52a6de />
             <ul class="multiple-selector__value">
-              <li @click="countv-for="language in values">{{language}}</li>
+              <li v-on:click="handleSelectorElementClick(language)" v-for="language in values">{{language}}</li>
             </ul>
-            <p class="multiple-selector__titles">Databases</p>
-            <hr width="85%" size="1" color=#52a6de />
-            <ul class="multiple-selector__value">
-              <li>MongoDB</li>
-              <li>MySQL</li>
-              <li>Oracle</li>
-            </ul>
-            <p class="multiple-selector__titles">Industries</p>
-            <hr width="85%" size="1" color=#52a6de />
-            <ul class="multiple-selector__value">
-              <li>Hospitality</li>
-              <li>Oil & Gas</li>
-              <li>Retail</li>
-              <li>Automotive</li>
-              <li>Human Resources</li>
-              <li>Life Sciences</li>
-            </ul>
-          </div>
+    </div>
   `,
-}
+  props: {
+    values: Array,
+    selected_value: String,
+    title: String
+  },
+  methods: {
+    countEntyOccurrences(projects, programming_languages) {
+      return projects.filter(element => element === programming_languages).length 
+    },
+    handleSelectorElementClick(value) {
+      // Assigns selected value to var in state
+      // this.selectedValue = value;
+      // Send data to parent
+      this.$emit('value-selected', this.title, value);
+  },
+})
